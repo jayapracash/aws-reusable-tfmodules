1e6b32d7bc49e7fc569c3907b1b3ba32723abd6c
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-13 13:53:05 +0530

    Update outputs.tf

--- modules/aws-ec2-instances/outputs.tf
+++ modules/aws-ec2-instances/outputs.tf
@@ -1,16 +1,6 @@
 # Output variable definitions
-
-output "arn" {
-  description = "ARN of the bucket"
-  value       = aws_s3_bucket.s3_bucket.arn
-}
-
 output "name" {
-  description = "Name (id) of the bucket"
-  value       = aws_s3_bucket.s3_bucket.id
+  description = "Name (id) of the ec2"
+  value       = aws_instance.ec2_instance.id
 }
 
-output "domain" {
-  description = "Domain name of the bucket"
-  value       = aws_s3_bucket_website_configuration.s3_bucket.website_domain
-}
