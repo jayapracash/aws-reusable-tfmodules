f53c96a4530feb625434757320f823c7495d7cb4
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-13 13:39:55 +0530

    Update main.tf

--- modules/aws-ec2-instances/main.tf
+++ modules/aws-ec2-instances/main.tf
@@ -1,23 +1,26 @@
 # Terraform configuration
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+resource "aws_instance" "ec2_instance" {
+    tags = var.tags
+                                                                                  # module "ec2_instance" {
+                                                                                    #   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "single-instance"
 
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  name = var.name 
+  instance_count = var.instance_count
+  ami                    = var.ami 
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
 }
-  
+--------------------------------------------
+    name           = "mydemosingleinstance"
+  instance_count = 1
+  ami                    = "ami-0c5204531f799e0c6"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+     
+--------------------------------------------
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 
