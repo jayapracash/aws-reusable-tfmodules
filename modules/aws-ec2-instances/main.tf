e37e3373611bd8f75975dd0e8654324393240168
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-13 13:43:23 +0530

    Update main.tf

--- modules/aws-ec2-instances/main.tf
+++ modules/aws-ec2-instances/main.tf
@@ -12,14 +12,4 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
   subnet_id              = var.subnet_id
 }
---------------------------------------------
-    name           = "mydemosingleinstance"
-  instance_count = 1
-  ami                    = "ami-0c5204531f799e0c6"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
-     
---------------------------------------------
-
 
