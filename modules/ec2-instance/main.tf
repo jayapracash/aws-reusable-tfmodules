3ec990405f8da960ab06fcff11b6bbe4da242100
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-14 23:10:19 +0530

    Update main.tf

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -3,6 +3,9 @@
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
 
   tags = {
     Name = "MysampleInstance"
