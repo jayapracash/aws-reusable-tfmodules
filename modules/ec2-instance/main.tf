557490ae30f1f7c28ddf83831d6daaed2b174c8a
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-17 15:34:13 +0530

    Update main.tf

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -3,14 +3,14 @@
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance
-  security_groups             = ["${module.aws_security_group.project-iac-sg.id}"]
+  security_groups             = ["${module.aws-security-group.project-iac-sg.id}"]
   subnet_id                   = "${aws_subnet.public-subnet-1.id}"
   associate_public_ip_address = true
 
   tags = {
     Name = "MysampleInstance"
   }
-  depends_on = [module.aws_security_group.project-iac-sg]
+  depends_on = [module.aws-security-group.project-iac-sg]
   lifecycle {
     create_before_destroy = true
   }
