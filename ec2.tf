62a81ee51fd48ded764401f4d8a24103be4aa086
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-17 15:32:24 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,10 @@
 #--- root/ ec2.tf ---
-
+module "aws-vpc-module" {
+  source = "./modules/vpc-module"
+}
+  module "aws-security-group" {
+  source = "./modules/security-group-module"
+}
 module "ec2-instance" {
   source = "./modules/ec2-instance"
- 
-  #depends_on = [aws_security_group.project-iac-sg]
-
 }
