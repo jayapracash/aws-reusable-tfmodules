5d313e33f291ad9442eebf9982a11155136c795d
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-17 16:14:24 +0530

    Update main.tf

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance
   associate_public_ip_address = true
-  subnet_id  = "${data.aws_subnet.selected.public-subnet-1.id}"
-  security_groups             = ["${data.aws-security-group.selected.id}"]
+  subnet_id  = "${data.aws_subnet.selected.id}"
+  security_groups             = ["${data.aws_security_group.selected.id}"]
 
   tags = {
     Name = "MysampleInstance"
