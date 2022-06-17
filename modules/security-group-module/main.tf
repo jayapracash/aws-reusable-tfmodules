1289f845171e1b9c9effa2b8f2765e9c9021c14e
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-17 15:22:44 +0530

    Create main.tf

---
+++ modules/security-group-module/main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "project-iac-sg" {
+  name        = "IAC-Sec-Group"
+  description = "MYIAC-Sec-Group"
+  //vpc_id      = module.aws_vpc.vpc.id 
+    //vpc_id    = aws_vpc.vpc.id
+
+  // To Allow SSH Transport
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
