01feb84a9d43aa522cbb16c05206706e424a5bcb
jpranayk <100947103+jpranayk@users.noreply.github.com>
2022-06-17 15:50:58 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,9 @@ module "aws-vpc-module" {
   module "aws-security-group" {
   source = "./modules/security-group-module"
 }
+
 module "ec2-instance" {
   source = "./modules/ec2-instance"
+  security_groups             = ["${module.aws-security-group.project-iac-sg.id}"]
+  subnet_id                   = "${data.aws_subnet.selected.public-subnet-1.id}"
 }
