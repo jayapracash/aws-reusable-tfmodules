e54c8a08666c2c6f8f571c631a431dcae0036c5e
Jayaprakash Bandari <jayaprakashbandarii@gmail.com>
2022-12-15 16:20:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 variable "vpc_name" {
   description = "Name of VPC"
   type        = string
-  default     = "example-vpc"
+  default     = "bb-vpc"
 }
 
 variable "vpc_cidr" {
@@ -15,19 +15,19 @@ variable "vpc_cidr" {
 variable "vpc_azs" {
   description = "Availability zones for VPC"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-west-2a", "us-west-2b"]
 }
 
 variable "vpc_private_subnets" {
   description = "Private subnets for VPC"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "vpc_public_subnets" {
   description = "Public subnets for VPC"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  default     = ["10.0.4.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
